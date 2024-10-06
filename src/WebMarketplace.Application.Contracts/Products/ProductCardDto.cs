cea9ff21ec92f4fbe3cfcda7104a1012e1d37447
xdevbh <bohdan.hutnykov@outlook.com>
2024-10-06 20:25:16 +0200

    feat: created Product Card and Product Card Deck

--- src/WebMarketplace.Application.Contracts/Products/ProductCardDto.cs
+++ src/WebMarketplace.Application.Contracts/Products/ProductCardDto.cs
@@ -7,6 +7,8 @@ public class ProductCardDto : EntityDto<Guid>
 {
     public string Name { get;  set; }
     public double Rating { get; set; }
+    public string? ShortDescription { get; set; }
+
     public decimal PriceAmount { get; set; }
     public string PriceCurrency { get; set; }
 }
